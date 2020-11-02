edebd595ffcdf63479027d76fb654f7c4fb45f1a
5QWSkduaMU@student.ethz.ch <5qwskduamu@student.ethz.ch>
2020-11-02 12:24:11 +0100

    Initial commit

---
+++ asvzsniper/main.py
@@ -0,0 +1,110 @@
+import urllib.request
+import json
+import datetime
+import argparse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+FACILITIES = {'Höngg': '45598',
+              'Irchel': '45577'}
+
+SPORTS = {'Fitness': '122920'}
+
+
+def main(timeslot, facility='Irchel'):
+    timetable_request = f'https://asvz.ch/asvz_api/event_search?f[0]=facility:{FACILITIES[facility]}&f[1]=sport:{SPORTS["Fitness"]}&availability=1&_format=json'
+    response = urllib.request.urlopen(timetable_request).read()
+    data = json.loads(response)
+    now = datetime.datetime.now()
+    if timeslot is None:
+        print('No timeslot provided, enrolling for the next possible slot')
+        entry = data['results'][0]
+
+    else:
+        timeslot = datetime.datetime.strptime(timeslot, '%Y.%m.%d-%H:%M')
+        for _entry in data['results']:
+            date = convert_asvz_time(_entry['from_date'])
+            if date == timeslot:
+                entry = _entry
+                break
+        else:
+            raise Exception(f'Could not find any slots at {timeslot}')
+
+    oe_date = convert_asvz_time(entry['oe_from_date'])
+    entry_url = entry['url']
+    places_free = entry['places_free']
+
+    if places_free == 0:
+        raise Exception(f'No more free slots at {timeslot}')
+
+    driver = init_driver()
+
+    login_xpath = """/html/body/app-root/div/div[2]/app-lesson-details/div/div/app-lessons-enrollment-button/button"""
+    try:
+        driver.get(entry_url)
+        login_button = driver.find_element_by_xpath(login_xpath)
+        title = login_button.get_attribute('title')
+        if title != 'Login':
+            raise NoSuchElementException
+    except NoSuchElementException:
+        pass
+    else:
+        driver.get('https://auth.asvz.ch/account/login')
+        WebDriverWait(driver, 360).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#LoggedInUser")))
+        print('Logged in!')
+    finally:
+        driver.close()
+
+    now = datetime.datetime.now()
+    print(f'Waiting for enrollment timeslot at {oe_date}')
+    while now < oe_date:
+        now = datetime.datetime.now()
+
+    driver = init_driver()
+    driver.get(entry_url)
+    enroll_button = driver.find_element_by_id('btnRegister')
+    html = enroll_button.get_attribute('innerHTML')
+    if 'ng-star-insterted' in html:
+        print('Already enrolled')
+    else:
+        enroll_button.click()
+        enroll_button = driver.find_element_by_id('btnRegister')
+        html = enroll_button.get_attribute('innerHTML')
+        if 'ng-star-insterted' in html:
+            print('Succesfully Enrolled')
+        else:
+            print('Oops, something went wrong')
+    driver.close()
+
+
+def convert_asvz_time(time_str):
+    dt_format = '%Y-%m-%dT%H:%M:%SZ'
+    return datetime.datetime.strptime(time_str, dt_format).replace(tzinfo=datetime.timezone.utc).astimezone(tz=None).replace(tzinfo=None)
+
+
+def init_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=chromeProfile")
+
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    driver.implicitly_wait(1)
+    return driver
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='ASVZ Enrollment Sniper')
+
+    parser.add_argument('--slot',
+                        help='Datetime of your desired slot in "YYYY.MM.DD-HH:MM "')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    parsed_args = parse()
+    main(timeslot=parsed_args.slot)
